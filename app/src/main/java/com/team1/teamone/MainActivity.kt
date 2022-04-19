ac53fb10ab32ac165effb896758e8bb9b4752e6a
Leebumju <dlqkawns@naver.com>
2022-04-19 12:35:09 +0900

    테스트용 커밋

--- app/src/main/java/com/team1/teamone/MainActivity.kt
+++ app/src/main/java/com/team1/teamone/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import com.team1.teamone.MainFragments.BookMarkFragment
 import com.team1.teamone.MainFragments.HomeFragment
 import com.team1.teamone.MainFragments.ProfileFragment
-
+//
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
