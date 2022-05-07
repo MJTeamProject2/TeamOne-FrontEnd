66a107165922e73993846d96a3f5069c215d183f
mopil1102 <mopil1102@gmail.com>
2022-05-07 15:22:53 +0900

    commit test

--- app/src/main/java/com/team1/teamone/network/RetrofitService.kt
+++ app/src/main/java/com/team1/teamone/network/RetrofitService.kt
@@ -74,7 +74,7 @@ interface RetrofitService {
     ): Call<BoardResponse>
 
     companion object { // static 처럼 공유객체로 사용가능함. 모든 인스턴스가 공유하는 객체로서 동작함.
-        private const val BASE_URL = "https://c7a3-121-136-173-243.jp.ngrok.io" // 주소
+        private const val BASE_URL = "http:10.0.2.2:8080" // 주소
 
         fun create(): RetrofitService {
             val gson = GsonBuilder().setLenient().create();
